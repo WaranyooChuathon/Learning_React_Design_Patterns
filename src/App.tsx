47c95c38abfd0c9882c84c594a7d65282358f929
WaranyooChuathon <waranyoo.chuathon@gmail.com>
2025-07-18 14:13:05 +0700

    List and Item

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 
 import { SplitScreen } from './LayoutComponents/Split_screen_components'
 import {SplitScreenImprovent} from './LayoutComponents/Split_screen_components_Improvement'
-import './App.css'
+
+import {RegularList} from './LayoutComponents/RegularList'
+import {LargePersonListItem} from './LayoutComponents/LargePersonListItem'
+import {SmallPersonListItem} from './LayoutComponents/SmallPersonListItem'
 
 interface MessageProps {
   message: string;
@@ -34,6 +37,41 @@ const Right = () => {
   )
 }
 
+const people = [{
+	name: 'John Doe',
+	age: 54,
+	hairColor: 'brown',
+	hobbies: ['swimming', 'bicycling', 'video games'],
+}, {
+	name: 'Brenda Smith',
+	age: 33,
+	hairColor: 'black',
+	hobbies: ['golf', 'mathematics'],
+}, {
+	name: 'Jane Garcia',
+	age: 27,
+	hairColor: 'blonde',
+	hobbies: ['biology', 'medicine', 'gymnastics'],
+}];
+
+const products = [{
+	name: 'Flat-Screen TV',
+	price: '$300',
+	description: 'Huge LCD screen, a great deal',
+	rating: 4.5,
+}, {
+	name: 'Basketball',
+	price: '$10',
+	description: 'Just like the pros use',
+	rating: 3.8,
+}, {
+	name: 'Running Shoes',
+	price: '$120',
+	description: 'State-of-the-art technology for optimum running',
+	rating: 4.2,
+}];
+
+
 
 function App() {
 
@@ -50,6 +88,28 @@ function App() {
         <RightHandComponent message="RIGHT!!" />
       </SplitScreenImprovent>
 
+
+      <RegularList 
+      items={people}
+      resourceName="person"
+      itemComponent={LargePersonListItem}
+      />
+
+      
+      <RegularList 
+      items={people}
+      resourceName="person"
+      itemComponent={SmallPersonListItem}
+      />
+
+      <RegularList
+        items={products}
+        resourceName="product"
+        itemComponent={LargePersonListItem}
+      />
+      
+
+   
 
     </>
   )
